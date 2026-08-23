c84ffd20607205c574e932668da4fe17ff38558f
prakashsahil987-droid <prakashsahil987@gmail.com>
2026-08-23 14:59:12 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@ import { structureTool } from "sanity/structure";
 import { visionTool } from "@sanity/vision";
 import { schemaTypes } from "./sanity/schemaTypes";
 
-const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!;
+const projectId = koi6wzx4;
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET!;
 
 export default defineConfig({
